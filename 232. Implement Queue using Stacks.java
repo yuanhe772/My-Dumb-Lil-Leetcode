1f567d12fe495f0a390703c7f224aec76c1b3146
yuanhe772 <31265339+yuanhe772@users.noreply.github.com>
2018-06-27 22:18:47 -0700

    Update 232. Implement Queue using Stacks.java

--- 232. Implement Queue using Stacks.java
+++ 232. Implement Queue using Stacks.java
@@ -25,3 +25,55 @@ You may assume that all operations are valid (for example, no pop or peek operat
 */
 
 
+class MyQueue {
+    
+    //For input
+    Deque<Integer> stack1;
+    //For output
+    Deque<Integer> stack2;
+    
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new LinkedList<Integer>();
+        stack2 = new LinkedList<Integer>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.addFirst(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.addFirst(stack1.removeFirst());
+            }
+        }
+        return stack2.removeFirst();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.addFirst(stack1.removeFirst());
+            }
+        }
+        return stack2.peekFirst();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
