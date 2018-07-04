f59319109366649d1044bd320c449fad8d7ed7ef
yuanhe772 <31265339+yuanhe772@users.noreply.github.com>
2018-07-04 01:35:35 -0700

    Update 144. Binary Tree Preorder Traversal.java

--- 144. Binary Tree Preorder Traversal.java
+++ 144. Binary Tree Preorder Traversal.java
@@ -8,18 +8,44 @@
  * }
  */
 class Solution {
-    
-    ArrayList<Integer> res = new ArrayList<Integer>();
-    
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
-        // recursive: 会一直recurse到null
-        if(root!=null){
-            res.add(root.val);
-            res.addAll(preorderTraversal(root.left));
-            res.addAll(preorderTraversal(root.right));
-        }
+//     // Recursive
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<Integer>();
         
-        return res;
-    }
+//         // 相当于base case只返回空的res
+        
+//         // recursive: 会一直recurse到null
+//         if(root!=null){
+//             res.add(root.val);
+//             res.addAll(preorderTraversal(root.left));
+//             res.addAll(preorderTraversal(root.right));
+//         }
+        
+//         return res;
+//     }
+    
+    
+    
+        // Iterative
+        public List<Integer> preorderTraversal(TreeNode root) {
+            
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            
+            while(!stack.empty() || root!=null){
+                if(root!=null){
+                    stack.push(root);
+                    res.add(root.val);
+                    root = root.left;
+                }
+                else{
+                    TreeNode t = stack.pop();
+                    root = t.right;
+                }
+            }
+            
+            return res;   
+        }
+    
+    
 }
