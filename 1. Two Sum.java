a1bf77b03a44f9942df4000d7325ef5a0bb04f0e
yuanhe772 <31265339+yuanhe772@users.noreply.github.com>
2018-07-08 03:45:05 -0700

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -5,22 +5,36 @@ You may assume that each input would have exactly one solution, and you may not 
 
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        //create a new array:int a[]=new int[10];
-        int[] r=new int[2];
-        int i=0, j=0;
-        //inside of for-loop, the first element in parenthesis has to be an expression
-        for(i=0;i<nums.length;i++){// arrays: array.length, String: string.length()
-            for(j=i+1;j<nums.length;j++){//cannot use i++!(it will increment i after assigned the value)
-                //cannot use ++i(it will change the value of i)
-                //can only use i+1!!! Because it wont change the value of i
-                if(nums[i]+nums[j]==target){
-                    r[0]=i;
-                    r[1]=j;
-                    return r;}
+    // 法一：O（n^2）Brute Force
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i=0; i<nums.length-1; i++){
+//             for(int j=i+1; j<nums.length; j++){ // has to be i+1 to avoid using the same element twice, e.g. [3,2,4] target == 6, 
+//                 if(nums[i]+nums[j] == target){
+//                     int[] res = {i,j};
+//                     return res;
+//                 }
+//             }
+//         }
+        
+//         int[] res = new int[0];
+//         return res;
+//     }
+    
+    // 法二：O（n）HashMap
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            if(map.get(target - nums[i]) != null){
+                int[] res = {i, map.get(target-nums[i])};
+                return res;
             }
+            map.put(nums[i], i);
         }
         
-        return r;//the return statement has to be there!!!
+        
+        int[] res = new int[0];
+        return res;
+        
     }
 }
