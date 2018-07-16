7d3ce9317f308f5282969728dcd90363864f6534
Yuan(Alex) He <yh772@cornell.edu>
2018-07-15 20:26:05 -0700

    Update 203. Remove Linked List Elements.java

--- 203. Remove Linked List Elements.java
+++ 203. Remove Linked List Elements.java
@@ -24,37 +24,39 @@ Output: 1->2->3->4->5
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         
-
-        
-//         // 法一：✅46.5%
-//         if(curr == null) return null;
-//         while(curr!=null && curr.val==val) curr = curr.next;
-//         // if(curr == null || curr.next == null && curr.val == val) return null;
-
-//         ListNode dummy = new ListNode(0);
-//         dummy.next = curr;
-//         while(curr!=null && curr.next!=null){
-//             if(curr.next.val == val) curr.next = curr.next.next;
-//             else curr = curr.next;
-//         }
-//         return dummy.next;
+        // // 法一：✅47.24%
+        // if(head == null) return null;
+        // while(head!=null && head.val==val) head = head.next;
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // while(head!=null && head.next!=null){
+        //     if(head.next.val == val) head.next = head.next.next; // 只是把next field变了，head，因为下一轮的next还可能重合
+        //     else head = head.next; // 如果下一个是安全的，head
+        // }
+        // return dummy.next;
         
         
+        // 法二：✅和上面一模一样但是利用浅拷贝 47.24%
+        if(head == null) return null;
+        while(head!=null && head.val == val) head = head.next;
+        ListNode curr = head;
+        while(curr!=null && curr.next != null){
+            if(curr.next.val == val) curr.next = curr.next.next;
+            else curr = curr.next;
+        } 
+        return head;
         
-        // 法二：✅ 抄的答案 47.34%
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        while(head != null){
-            if(head.val == val){
-                prev.next = head.next;
-            }
-            else{
-                prev = prev.next;
-            }
-            head = head.next;
-        }
-        return dummy.next;
         
+        
+        // // 法三：✅ 抄的答案 47.34%
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // ListNode prev = dummy;
+        // while(head != null){
+        //     if(head.val == val) prev.next = head.next;
+        //     else prev = prev.next;
+        //     head = head.next;
+        // }
+        // return dummy.next;
     }
 }
