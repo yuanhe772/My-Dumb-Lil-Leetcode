6fb1db159f6f4078cfa3ee9c9ad2b31d3c2dfc10
Yuan(Alex) He <yh772@cornell.edu>
2018-07-17 03:35:20 -0700

    Update 144. Binary Tree Preorder Traversal.java

--- 144. Binary Tree Preorder Traversal.java
+++ 144. Binary Tree Preorder Traversal.java
@@ -8,44 +8,50 @@
  * }
  */
 class Solution {
-//     // Recursive
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> res = new ArrayList<Integer>();
-        
-//         // 相当于base case只返回空的res
-        
-//         // recursive: 会一直recurse到null
-//         if(root!=null){
-//             res.add(root.val);
-//             res.addAll(preorderTraversal(root.left));
-//             res.addAll(preorderTraversal(root.right));
-//         }
-        
-//         return res;
-//     }
+    // // Recursive        100%
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> res = new ArrayList<Integer>();
+    //     // 相当于base case只返回空的res
+    //     // recursive: 会一直recurse到null
+    //     if(root!=null){
+    //         res.add(root.val);
+    //         res.addAll(preorderTraversal(root.left));
+    //         res.addAll(preorderTraversal(root.right));
+    //     }
+    //     return res;
+    // }
     
     
     
-        // Iterative
-        public List<Integer> preorderTraversal(TreeNode root) {
-            
-            ArrayList<Integer> res = new ArrayList<Integer>();
-            Stack<TreeNode> stack = new Stack<TreeNode>();
-            
-            while(!stack.empty() || root!=null){
-                if(root!=null){
-                    stack.push(root);
-                    res.add(root.val);
-                    root = root.left;
-                }
-                else{
-                    TreeNode t = stack.pop();
-                    root = t.right;
-                }
-            }
-            
-            return res;   
+        // // Iterative        96.23%
+        // public List<Integer> preorderTraversal(TreeNode root) {
+        //     ArrayList<Integer> res = new ArrayList<Integer>();
+        //     Stack<TreeNode> stack = new Stack<TreeNode>();
+        //     while(!stack.empty() || root!=null){
+        //         if(root!=null){
+        //             stack.push(root);
+        //             res.add(root.val);
+        //             root = root.left;
+        //         }
+        //         else{
+        //             TreeNode t = stack.pop();
+        //             root = t.right;
+        //         }
+        //     }
+        //     return res;   
+        // }
+    
+    //Recursive2    100%
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        helper(res, root);
+        return res;
+    }
+    public void helper(ArrayList<Integer> arr, TreeNode n){
+        if(n!=null){
+            arr.add(n.val);
+            helper(arr, n.left);
+            helper(arr, n.right);
         }
-    
-    
+    }
 }
