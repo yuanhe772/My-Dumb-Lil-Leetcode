c6d1aca8832d6ea11f55a8ecef75d7fafe05814c
Yuan(Alex) He <yh772@cornell.edu>
2018-07-17 03:36:00 -0700

    Update 94. Binary Tree Inorder Traversal.java

--- 94. Binary Tree Inorder Traversal.java
+++ 94. Binary Tree Inorder Traversal.java
@@ -8,36 +8,47 @@
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        /**
+//     // // Iterative
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         /**
         
-        When
-              1
-             / \
-            /   \
-          null  null
+//         When
+//               1
+//              / \
+//             /   \
+//           null  null
           
-        Then the left null pops node 1, the right null pops the parent node of node 1.
+//         Then the left null pops node 1, the right null pops the parent node of node 1.
         
-        */
-        
-        
-        Stack<TreeNode> stack = new Stack<>();
+//         */
+//         // Stack<TreeNode> stack = new Stack<>();
+//         // ArrayList<Integer> res = new ArrayList<>();
+//         // while(!stack.empty() || root!=null){
+//         //     if(root != null){
+//         //         stack.push(root);
+//         //         root = root.left;
+//         //     }
+//         //     else{
+//         //         TreeNode t = stack.pop();
+//         //         res.add(t.val);
+//         //         root = t.right;
+//         //     }
+//         // }
+//         // return res;
+//     }
+    
+    //Recursive
+    public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> res = new ArrayList<>();
-        
-        while(!stack.empty() || root!=null){
-            if(root != null){
-                stack.push(root);
-                root = root.left;
-            }
-            else{
-                TreeNode t = stack.pop();
-                res.add(t.val);
-                root = t.right;
-            }
-        }
-        
+        helper(res, root);
         return res;
-        
     }
+    public void helper(ArrayList<Integer> arr, TreeNode n){
+        if(n!=null){
+            helper(arr, n.left);
+            arr.add(n.val);
+            helper(arr, n.right);
+        }
+    }
+    
 }
