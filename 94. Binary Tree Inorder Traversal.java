33ac619aac2ac86bb53acccd3c20da00e59609b4
Yuan(Alex) He <yh772@cornell.edu>
2018-07-17 03:40:22 -0700

    Update 94. Binary Tree Inorder Traversal.java

--- 94. Binary Tree Inorder Traversal.java
+++ 94. Binary Tree Inorder Traversal.java
@@ -8,47 +8,47 @@
  * }
  */
 class Solution {
-//     // // Iterative
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         /**
-        
-//         When
-//               1
-//              / \
-//             /   \
-//           null  null
-          
-//         Then the left null pops node 1, the right null pops the parent node of node 1.
-        
-//         */
-//         // Stack<TreeNode> stack = new Stack<>();
-//         // ArrayList<Integer> res = new ArrayList<>();
-//         // while(!stack.empty() || root!=null){
-//         //     if(root != null){
-//         //         stack.push(root);
-//         //         root = root.left;
-//         //     }
-//         //     else{
-//         //         TreeNode t = stack.pop();
-//         //         res.add(t.val);
-//         //         root = t.right;
-//         //     }
-//         // }
-//         // return res;
-//     }
-    
-    //Recursive
+    // Iterative        95.87%
     public List<Integer> inorderTraversal(TreeNode root) {
+        /**
+        
+        When
+              1
+             / \
+            /   \
+          null  null
+          
+        Then the left null pops node 1, the right null pops the parent node of node 1.
+        
+        */
+        Stack<TreeNode> stack = new Stack<>();
         ArrayList<Integer> res = new ArrayList<>();
-        helper(res, root);
+        while(!stack.empty() || root!=null){
+            if(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            else{
+                TreeNode t = stack.pop();
+                res.add(t.val);
+                root = t.right;
+            }
+        }
         return res;
     }
-    public void helper(ArrayList<Integer> arr, TreeNode n){
-        if(n!=null){
-            helper(arr, n.left);
-            arr.add(n.val);
-            helper(arr, n.right);
-        }
-    }
+    
+    // //Recursive     100%
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     ArrayList<Integer> res = new ArrayList<>();
+    //     helper(res, root);
+    //     return res;
+    // }
+    // public void helper(ArrayList<Integer> arr, TreeNode n){
+    //     if(n!=null){
+    //         helper(arr, n.left);
+    //         arr.add(n.val);
+    //         helper(arr, n.right);
+    //     }
+    // }
     
 }
