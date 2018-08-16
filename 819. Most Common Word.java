c42ee162b6a562697fc481e890888478b4718f68
Yuan(Alex) He <yh772@cornell.edu>
2018-08-15 21:04:25 -0700

    Update 819. Most Common Word.java

--- 819. Most Common Word.java
+++ 819. Most Common Word.java
@@ -34,6 +34,8 @@ Words only consist of letters, never apostrophes or other punctuation symbols.
 
 
 class Solution {
+    
+    // 法一：我的笨方法：            10.39%
     public String mostCommonWord(String paragraph, String[] banned) {
         // Set<String> ban = new HashSet<>(banned.asList(someArray));
         
@@ -43,9 +45,46 @@ class Solution {
             ban.add(banned[i]);
         }
         
-        String[] para = (paragraph.toLowerCase).split(" ",);
+        // String[] para = (paragraph.toLowerCase()).split(" |\\.|,");
+        String[] para = (paragraph.toLowerCase()).split("\\W");
         
+        Set<String> s = new HashSet<>();
+        Map<String, Integer> m = new HashMap<>();
+        for(int i=0; i<para.length; i++){
+            if(!ban.contains(para[i])){
+                /* 
+                if(s.add(para[i])) 
+                    m.put(para[i],1);
+                else
+                    m.put(para[i], m.get(para[i])+1);
+                上面这一大堆可以合并成下面这样：
+                */
+                m.put(para[i], m.getOrDefault(para[i],0)+1);
+            }
+        }
+        
+        int max = 0;
+        String maxStr = "";
+        
+        Iterator it = m.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry item = (Map.Entry)it.next();
+            // System.out.println((String)item.getKey() + "   " + (int)item.getValue());
+            int ct = (int) item.getValue();
+            String str = (String)item.getKey();
+            
+            if(ct>max && str.length()!=0 && str!=null){
+                // System.out.println((String)item.getKey() + "   " + (int)item.getValue());
+                max = ct;
+                maxStr = (String)item.getKey();
+            }  
+//             m.remove(item.getKey()); 对于iterator来说不需要remove
+        }
 
+        return maxStr;
+         /**
+        "Bob. hIt, baLl"
+        ["bob", "hit"]
+        */
     }
 }
-
