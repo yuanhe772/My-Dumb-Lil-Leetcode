1912b2de14db00aed945fff9814dd34d7ef0c91a
Yuan(Alex) He <yh772@cornell.edu>
2018-08-30 00:31:30 -0700

    Update 746. Min Cost Climbing Stairs.java

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -19,3 +19,23 @@ Every cost[i] will be an integer in the range [0, 999].
 
 
 */
+
+
+
+
+
+class Solution {
+    
+    // 30.79%
+    public int minCostClimbingStairs(int[] cost) { 
+        // 想象成小时候上台阶的样子 要么一步两节 要么一步一节
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<cost.length; i++){
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+        return Math.min(dp[cost.length-1], dp[cost.length-2]);
+        
+    }
+}
