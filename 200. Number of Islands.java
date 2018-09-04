6d7045c61c00d6714d64a9e3afc0a973bb17702b
Yuan(Alex) He <yh772@cornell.edu>
2018-09-03 20:52:50 -0700

    Update 200. Number of Islands.java

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -24,3 +24,38 @@ Output: 3
 
 
 */
+
+
+
+
+class Solution {
+    // 这道题就是说，数出一共有多少片有1的面积
+    // 做法就是，看到一个1，就把它周围的1（也包括它自己）都置为0，然后数图中一共有多少个1
+    
+    // 法一：DFS           95.93%
+    public int numIslands(char[][] grid) {
+        
+        int res = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1'){
+                    res++;
+                    dfs(grid,i,j);//把周围和自己都置0
+                }
+            }
+        }
+        return res;
+        
+    }
+    
+    public void dfs(char[][] grid, int r, int c){
+        int height = grid.length; //就是有多少行
+        int width = grid[0].length; //就是有多少列
+        if(r<0 || r>=height || c<0 || c>=width || grid[r][c]=='0') return; // 碰到0就返回，没有机会继续dfs的recurse
+        grid[r][c] = '0';
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+    }
+}
