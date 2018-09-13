70ac415152bbe340bc6dabf2f89e5866b52c839b
Yuan(Alex) He <yh772@cornell.edu>
2018-09-13 02:32:19 -0700

    Create 599. Minimum Index Sum of Two Lists.java

---
+++ 599. Minimum Index Sum of Two Lists.java
@@ -0,0 +1,61 @@
+/*
+
+Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+Example 1:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+Output: ["Shogun"]
+Explanation: The only restaurant they both like is "Shogun".
+Example 2:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["KFC", "Shogun", "Burger King"]
+Output: ["Shogun"]
+Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+Note:
+The length of both lists will be in the range of [1, 1000].
+The length of strings in both lists will be in the range of [1, 30].
+The index is starting from 0 to the list length minus 1.
+No duplicates in both lists.
+
+*/
+
+class Solution {
+    
+    // Yelp面经里的题目：56.53%
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        
+        Map<String, Integer> map1 = new HashMap<>();
+        
+        for(int i=0; i<list1.length; i++){
+            map1.put(list1[i], i);
+        }
+        
+        Map<String, Integer> map2 = new HashMap<>();
+        for(int i=0; i<list2.length; i++){
+            if(map1.containsKey(list2[i])){
+                map2.put(list2[i], i+map1.get(list2[i]));
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(Map.Entry<String, Integer> entry: map2.entrySet()){
+            if(entry.getValue()<min){
+                min = entry.getValue();
+            }
+        }
+        
+        List<String> res = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry: map2.entrySet()){
+            if(entry.getValue()==min){
+                res.add(entry.getKey());
+            }
+        }
+        
+        return res.toArray(new String[res.size()]);
+    }
+}
