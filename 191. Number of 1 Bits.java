6bb0444a36ac52a904653b9e0593e3fdf428eace
Yuan(Alex) He <yh772@cornell.edu>
2018-09-18 02:16:15 -0700

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -33,4 +33,16 @@ public class Solution {
         }
         return count;
     }
+    
+    //法三：mask移位 99.6%
+    public int hammingWeight(int n) {
+        int res = 0;
+        int mask = 1;
+        for(int i=0; i<32; i++){
+            if((n & mask) != 0){ res++; }
+            // n = n << 1;
+            mask <<= 1;
+        }
+        return res;
+    }
 }
