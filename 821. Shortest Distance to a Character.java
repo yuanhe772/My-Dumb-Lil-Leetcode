73d02346420240f6fb15ba49968900b749200768
Yuan(Alex) He <yh772@cornell.edu>
2018-09-30 04:08:28 -0700

    Update 821. Shortest Distance to a Character.java

--- 821. Shortest Distance to a Character.java
+++ 821. Shortest Distance to a Character.java
@@ -23,22 +23,19 @@ class Solution {
     // 从左往右看有最短距离，从右往左看也有最短距离，只要从这两个方向中选出来最小的就可以        100%
     // 如果不想brute force的话：只需要记住前一次看到的C在哪里就可以得出第i位与C的距离
     public int[] shortestToChar(String S, char C) {
-        int prev = Integer.MIN_VALUE/2, len = S.length();
+        int len = S.length(), prev = -len;
         int res[] = new int[len];
         // Left to right
         for(int i=0; i<len; i++){
-            if(S.charAt(i) == C){
+            if(S.charAt(i) == C)
                 prev = i;
-            }
             res[i] = i-prev;
         }
         // Right to left
-        prev = Integer.MAX_VALUE/2;
         for(int i=len-1; i>=0; i--){
-            if(S.charAt(i) == C){
+            if(S.charAt(i) == C)
                 prev = i;
-            }
-            res[i] = Math.min(prev-i, res[i]);
+            res[i] = Math.min(Math.abs(i-prev), res[i]);
         }
         return res;
     }
