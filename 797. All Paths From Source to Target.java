beeaad4ab674d3b34f529b75fe20a06e9787c4c1
Yuan(Alex) He <yh772@cornell.edu>
2018-09-30 03:31:28 -0700

    Update 797. All Paths From Source to Target.java

--- 797. All Paths From Source to Target.java
+++ 797. All Paths From Source to Target.java
@@ -23,8 +23,7 @@ You can print different paths in any order, but you should keep the order of nod
 
 
 class Solution {
-    
-    // wcnm的这题好难懂： ✅         100%
+    // wcnm的这题题目好难懂： ✅         100%
     // 对于[[1,2], [3], [3], []] 意思就是0节点是指向1，2的，1节点是指向3的，2节点是指向3的
     // 也就是说index是表示xx节点的，然后那个节点上的值就是节点指向的地方
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
@@ -38,10 +37,13 @@ class Solution {
         }
         else{
             for(int i=0; i<graph[node].length; i++){
+                // 这里只当做一个statement，不当做一个execution的赋值操作，所以，在这里return了的结果不用管
                 dfs(graph, graph[node][i], path, res);
             }
         }
         //没有这句话的话， 结果就成了 [[0,1,3],[0,1,3,2,3]]，这句话起到了backTrack的作用
+        //只有这里把最后一个node删除了，return res的时候返回到dfs那里的时候，才会继续重新开始加新的node
+        //所以当recursion执行到最后，实际上path=[]
         path.remove(path.size()-1);
         return res;
     }
