7df81af8d68229cf04359c1ade5027a9f933b573
Yuan(Alex) He <yh772@cornell.edu>
2018-11-26 03:18:02 -0800

    Create 394. Decode String.java

---
+++ 394. Decode String.java
@@ -0,0 +1,38 @@
+class Solution {
+    // 法一：如果stack是空的就prepend到结果里，如果不是空的，就append到stack的最上面的元素后面
+    // 法二：这种想法就是直接重复res，93.7%
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
+    }
+}
