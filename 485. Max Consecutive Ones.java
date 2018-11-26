a7b0db42b16b84e396c57a83bef48e243357a8ef
Yuan(Alex) He <yh772@cornell.edu>
2018-11-26 00:18:35 -0800

    Create 485. Max Consecutive Ones.java

---
+++ 485. Max Consecutive Ones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = count;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1) count ++;
+            else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        return Math.max(max, count);
+    }
+}
