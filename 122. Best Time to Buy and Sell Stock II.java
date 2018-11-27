a298aae822ecff48e34471d4e4df6bcf2bf7b31a
Yuan(Alex) He <yh772@cornell.edu>
2018-11-27 03:52:44 -0500

    Update 122. Best Time to Buy and Sell Stock II.java

--- 122. Best Time to Buy and Sell Stock II.java
+++ 122. Best Time to Buy and Sell Stock II.java
@@ -62,4 +62,17 @@ class Solution {
             
             
     }
+  
+  // 法二：DP2：画个图就出来了
+  public int maxProfit(int[] prices) {
+      if(prices == null || prices.length == 0) return 0;
+      int min = prices[0];
+      int[] dp = new int[prices.length];
+      dp[0] = 0;
+      for(int i=1; i<prices.length; i++){
+          dp[i] = Math.max(dp[i-1], prices[i] - min);
+          min = Math.min(prices[i], min);
+      }
+      return dp[prices.length-1];
+  }
 }
