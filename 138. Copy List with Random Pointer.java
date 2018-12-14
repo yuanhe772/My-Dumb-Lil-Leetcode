8be0e6c97de9e736ee23793c23c2578cd73c8719
Yuan(Alex) He <yh772@cornell.edu>
2018-12-14 06:23:17 -0800

    Update 138. Copy List with Random Pointer.java

--- 138. Copy List with Random Pointer.java
+++ 138. Copy List with Random Pointer.java
@@ -17,13 +17,14 @@ public class Solution {
         
         // 法二：用HashMap去存储copy完的node，然后重建copied node list
         HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
-        // 1. copy all the node:
+        // 1. copy all the node:   一开始先把没有任何pointer的，只是复制了原来node的value的这些copied node 全部放进hashMap
         RandomListNode curr = head;
         while(curr!=null){
             map.put(curr, new RandomListNode(curr.label));
             curr = curr.next;
         }
-        // 2. arrange copied node's fields of next and random:
+        // 2. arrange copied node's fields of next and random:   然后让copied node（通过原node索引来的！）的指针指向被指向的那个node索引着的copied node值
+        // Random 指针同理
         RandomListNode dummy = new RandomListNode(0);
         dummy.next = map.get(head);
         while(head!=null){
